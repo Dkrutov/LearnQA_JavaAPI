67c4bda3081d26c098d36764efc05f6d330c53a6
Dmitrii Krutov <dm.krutov@mail.ru>
2024-12-19 19:25:57 +0300

    Добавлен параметр params

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,11 +2,18 @@ import  io.restassured.RestAssured;
 import  io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld() {
-//        System.out.println("HelloWorld");
+    public void testRestAssured() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name","John");
+
         Response response = RestAssured
+                .given()
+                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
