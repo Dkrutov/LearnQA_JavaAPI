b558cd4d7f6745a8f000f5078bae929f40365f3f
Dmitrii Krutov <dm.krutov@mail.ru>
2024-12-26 23:06:46 +0300

    Тесты testCookie и testHeader

--- src/test/java/JunitHomeWork.java
+++ src/test/java/JunitHomeWork.java
@@ -1,4 +1,8 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,12 +17,18 @@ public class JunitHomeWork {
 
     @Test
     public void testCookie() {
-
+        Response responseHomeWorkCookie = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        assertEquals("hw_value",responseHomeWorkCookie.getCookie("HomeWork"), "Unexpected cookie");
     }
 
     @Test
     public void testHeader() {
-
+        Response responseHomeWorkCookie = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        assertEquals("Some secret value",responseHomeWorkCookie.getHeader("x-secret-homework-header"), "Unexpected x-secret-homework-header:");
     }
 
 
