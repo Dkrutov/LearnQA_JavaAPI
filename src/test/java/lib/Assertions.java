06512de0e2b360b2fe08a13ac2ad18a935a7118d
Dmitrii Krutov <dm.krutov@mail.ru>
2025-01-04 15:50:01 +0300

    Тест testEditJustCreatedTest

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -14,6 +14,13 @@ public class Assertions {
         assertEquals(expectedValue,value,"JSON value is not eaual to expected value");
     }
 
+    public static void asserJsonByName(Response Response, String name, String expectedValue){
+        Response.then().assertThat().body("$",hasKey(name));
+
+        String value = Response.jsonPath().getString(name);
+        assertEquals(expectedValue,value,"JSON value is not eaual to expected value");
+    }
+
     public static void assertResponseTextEquals(Response Response, String expectedAnswer) {
         assertEquals(
                 expectedAnswer,
