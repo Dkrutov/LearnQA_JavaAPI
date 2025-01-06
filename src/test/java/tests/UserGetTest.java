c88fdc4806a29aea76e5cbe9d3aaa1fe74a3e0bf
Dmitrii Krutov <dm.krutov@mail.ru>
2025-01-06 21:14:04 +0300

    Тест testGetUserDetailsAuthAsAnotherUser

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -1,15 +1,24 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.Assertions;
 import lib.BaseTestCase;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Epic("Information verification cases")
+@Feature("Information verification")
 public class UserGetTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
 
     @Test
     public void testGetUserDataNotAuth() {
@@ -45,6 +54,28 @@ public class UserGetTest extends BaseTestCase {
                 .andReturn();
         String[] expectedFields = {"username","firstName","lastName","email"};
         Assertions.assertJsonHasFields(responseUserData,expectedFields);
+    }
+
+    @Test
+    @Description("This test successfully information verification user details auth as another user")
+    @DisplayName("Test positive information verification")
+    public void testGetUserDetailsAuthAsAnotherUser() {
+        Map<String,String> authData = new HashMap<>();
+        authData.put("email","vinkotov@example.com");
+        authData.put("password","1234");
+        Response responseGetAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/login",authData);
+        System.out.println(responseGetAuth.asString());
+
+        String header = this.getHeader(responseGetAuth,"x-csrf-token");
+        String cookies = this.getCookie(responseGetAuth,"auth_sid");
+
+        Response responseUserData = apiCoreRequests.makeGetRequest("https://playground.learnqa.ru/api/user/3",header,cookies);
+        System.out.println(responseUserData.asString());
+
+        Assertions.assertJsonHasField(responseUserData,"username");
+        Assertions.assertJsonHasNotField(responseUserData,"firstName");
+        Assertions.assertJsonHasNotField(responseUserData,"lastName");
+        Assertions.assertJsonHasNotField(responseUserData,"email");
     }
 
 }
